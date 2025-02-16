d0c8e924ff99fcbf78eae593bd9b443f7d7ff50b
BVK Ratnesh <ratneshbvk2504@gmail.com>
2025-02-16 12:55:24 +0530

    Creating and styling End Screen

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,21 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
+import Modal from "./Modal";
 
 function Wordle({ solution }) {
   const { currentGuess, handleKeyPress, guesses, isCorrect, turn, usedKeys } =
     useWordle(solution);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(
     function () {
       window.addEventListener("keyup", handleKeyPress);
 
       if (isCorrect) {
+        setTimeout(() => setShowModal(true), 2000);
         window.removeEventListener("keyup", handleKeyPress);
       }
 
       if (turn > 5) {
+        setTimeout(() => setShowModal(true), 2000);
         window.removeEventListener("keyup", handleKeyPress);
       }
 
@@ -30,6 +34,9 @@ function Wordle({ solution }) {
       <div>Current guess - {currentGuess}</div>
       <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
       <Keypad usedKeys={usedKeys} />
+      {showModal && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
     </div>
   );
 }
