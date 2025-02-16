2748f2d1eb5c8f422d88015ced7d98c7290ac07a
BVK Ratnesh <ratneshbvk2504@gmail.com>
2025-02-16 11:51:42 +0530

    Adding json-server and fetching words

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [answer, setAnswer] = useState(null);
+
+  useEffect(
+    function () {
+      async function getWords() {
+        try {
+          const res = await fetch("http://localhost:8080/solutions");
+          const data = await res.json();
+          // console.log(data);
+          // selecting a random word from the list of words
+          const randomWord = data[Math.floor(Math.random() * data.length)];
+          setAnswer(randomWord.word);
+          // console.log(randomWord);
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+      getWords();
+    },
+    [setAnswer]
+  );
+
   return (
     <div className="App">
-      <h1>Wordle (Lingo)</h1>
+      <h1>Wordle</h1>
+      {answer && <div>Word is: {answer} </div>}
     </div>
   );
 }
 
-export default App
+export default App;
 
 /* 
 
